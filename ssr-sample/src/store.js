ff83c9819ef7edd30ed51e55284086ff900bff68
NumanArshad <mnumanarshad@yahoo.com>
2020-08-07 18:43:07 +0500

    persist redux state

--- ssr-sample/src/store.js
+++ ssr-sample/src/store.js
@@ -1,5 +1,27 @@
 import rootReducer from './myindex'
 import {createStore} from 'redux'
 
-const store=createStore(rootReducer)
+const store=createStore(rootReducer,getStateFromLocalStorage())
+function saveToLocalStorage(state){
+    try{
+     localStorage.setItem('state',JSON.stringify(state))
+    }
+    catch(e){
+        console.log("save state error"+e)
+    }
+}
+function getStateFromLocalStorage(){
+    const serializeState=localStorage.getItem('state')
+
+    try{
+        if(serializeState===null) return undefined
+
+      return JSON.parse(serializeState)        
+    }
+    catch(e){
+        console.log("error"+e)
+        return undefined
+    }
+}
+store.subscribe(()=>saveToLocalStorage(store.getState()))
 export default store
